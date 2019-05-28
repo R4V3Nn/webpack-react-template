abcc5bf22f1af8277fe7b0f297c30ca0701e8d55
Artem Kaseka <artem.kaseka@synapse.com>
2019-05-28 16:43:03 +0300

    initial commit

---
+++ src/component/app/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './app.scss';
+
+const App = () => (
+	<div className="full-screen">
+		<div>
+			<h1>
+          React Template
+			</h1>
+			<h2>
+			Webpack, Babel, Eslint, React, Redux, SCSS
+			</h2>
+		</div>
+	</div>
+);
+
+export default App;
